bdbf4ef1cb0c64fd4e73d58ab989ca797571bde9
fv220 <fv220@ic.ac.uk>
2021-12-21 16:19:43 +0100

    feat: config keys setup

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb+srv://saxophone:saxophone2019@libridoo-brmvb.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+const db_secret = require("./config/keys").MONGO_URI;
 
 const app = express();
 
